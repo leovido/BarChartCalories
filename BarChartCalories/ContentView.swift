bf59df85a444a09be7caaf4c2736add7608360dc
Christian Ray Leovido <christian.leovido@gmail.com>
2020-03-25 12:13:06 +0000

    Initial Commit

---
+++ BarChartCalories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarChartCalories
+//
+//  Created by Christian Leovido on 25/03/2020.
+//  Copyright © 2020 Christian Leovido. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
